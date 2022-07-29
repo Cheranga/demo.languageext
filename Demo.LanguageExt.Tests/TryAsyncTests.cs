bd61233bba9bd3f59c9ebd5777177d6d5b11f35f
Cheranga Hatangala <chatangala@bunnings.com.au>
2022-07-29 20:57:37 +1000

    feat: refactored tryasync operations to be better [ci]

---
+++ Demo.LanguageExt.Tests/TryAsyncTests.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using LanguageExt;
+using LanguageExt.Common;
+using Newtonsoft.Json;
+using static LanguageExt.Prelude;
+
+namespace Demo.LanguageExt.Tests;
+
+public class TryAsyncTests
+{
+    [Fact]
+    public async Task Test()
+    {
+        TryAsync<string> ReadFileAsync(string filePath) =>
+            TryAsync(async () =>
+            {
+                if (!File.Exists(filePath))
+                {
+                    throw new FileNotFoundException("file not found", filePath);
+                }
+
+                return await File.ReadAllTextAsync(filePath);
+            });
+
+        TryAsync<Employee> ProcessContentAsync(string content) =>
+            TryAsync(() =>
+            {
+                if (string.IsNullOrWhiteSpace(content))
+                {
+                    throw new Exception("invalid data");
+                }
+
+                var employee = JsonConvert.DeserializeObject<Employee>(content);
+                if (string.IsNullOrWhiteSpace(employee?.Id) || string.IsNullOrWhiteSpace(employee?.Name))
+                {
+                    throw new Exception("employee data is invalid");
+                }
+
+                return employee.AsTask();
+            });
+
+
+        await ReadFileAsync("TestData/valid-employee.json").Bind(ProcessContentAsync).Match(
+            Succ: employee => { employee.Should().NotBeNull(); },
+            Fail: exception => { exception.Should().BeNull(); });
+
+        await ReadFileAsync("TestData/file-does-not-exist.json").Bind(ProcessContentAsync).Match(
+            Succ: employee => { employee.Should().BeNull(); },
+            Fail: exception => { exception.Should().BeOfType<FileNotFoundException>(); });
+
+        await ReadFileAsync("TestData/invalid-employee.json").Bind(ProcessContentAsync).Match(
+            Succ: employee => { employee.Should().BeNull(); },
+            Fail: exception =>
+            {
+                exception.Should().BeOfType<Exception>();
+                exception.Message.Should().Be("employee data is invalid");
+            });
+
+        // if you would like to return from the above operations, you will need to obviously return a single type, for that you can do the below
+        var operation = await ReadFileAsync("TestData/valid-employee.json")
+            .Bind(ProcessContentAsync)
+            .Match(
+                Either<Error, Employee>.Right,
+                exception => Either<Error, Employee>.Left(Error.New(exception)));
+
+        operation.IsRight.Should().BeTrue();
+        operation.IfRight(employee => employee.Should().NotBeNull());
+    }
+}
